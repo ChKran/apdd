a278a3501fac8d4b02286f85d18795a9a4bd1f1d
ChKran <kranasch@gmail.com>
2018-11-20 21:27:02 +0200

    subArray

--- subArray.cpp
+++ subArray.cpp
@@ -9,23 +9,6 @@
 
 using namespace std;
 
-/*
-int sumArray(int a[], int n){
-    int sum = 0;
-    for(int i=0;i<n;i++){
-        sum = sum + a[i];
-    }
-    return sum;
-}
-
-
-int SortArray(int a[], int n){
-    sort(a,n+a);
-    for(int i=0;i<n;i++)
-        cout<<a[i]<< " ";
-
-}
-*/
 
 int maxSubArray3(int a[], int n){ //Time Complexity O(n^#)
     
